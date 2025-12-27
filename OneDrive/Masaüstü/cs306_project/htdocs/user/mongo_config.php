395197c6d9fa318eb01070fd2faab56abf7b0c0c
dorukkocaman <doruk.kocaman@sabanciuniv.edu>
2025-12-27 20:53:18 +0300

    update mongo_config.php
    Removed unnecessary comments regarding MongoDB connection setup.

--- OneDrive/Masaüstü/cs306_project/htdocs/user/mongo_config.php
+++ OneDrive/Masaüstü/cs306_project/htdocs/user/mongo_config.php
@@ -1,7 +1,5 @@
 <?php
-// require_once __DIR__ . '/vendor/autoload.php'; // Not needed if using extension directly
-// Instructions say: "You are expexted to use MongoDB\Driver\Manager class"
-// So I will use the driver directly without composer autoload if possible, or assume extension is loaded.
+
 
 try {
     $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
